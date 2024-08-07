75b79be13948c8591695a78c33490ca55459b88f
Thomas <thomascode92@hotmail.com>
2024-08-07 21:29:37 +0200

    Enable Serial Communication

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED is ON");
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED is OFF");
   delay(1000);
 }
