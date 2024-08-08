280b83edd67d58d0a358a405187f16fe7a2819dd
Thomas <thomascode92@hotmail.com>
2024-08-08 20:22:18 +0200

    Print "Hello, World!" to the LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  lcd.begin(16, 2);
+  lcd.print("Hello, World!");
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("LED is ON");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("LED is OFF");
-  delay(1000);
+  lcd.noCursor(); // turn off the cursor
+  delay(500);
+
+  lcd.cursor(); // turn on the cursor
+  delay(500);
 }
